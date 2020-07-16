99ebbbb6187e47c52fe3d7c4277b4b3302f17443
Nataniel Tsimerman <deviln3107@hotmail.com>
2020-07-16 23:53:54 +0300

    First Changes
    - Added Empty Objects for Buildings and Street Lights.
    - Changed EnviromentControl script to work with new spawn mechanic.
    - Changed Buildings and Street Lights to the new Low Poly assets

--- Assets/Scripts/EnvironmentControl.cs
+++ Assets/Scripts/EnvironmentControl.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnvironmentControl : MonoBehaviour {
 
-    float spawnCycle = 0.08f;
+    [SerializeField]float spawnCycle = 0.08f;
     float spawnTimer = 0.0f;
     float lightsSpawnCycle = 0.8f;
     float lightsSpawnTimer = 0;
@@ -13,8 +13,12 @@ public class EnvironmentControl : MonoBehaviour {
     public GameObject[] allBuildingsPrefabs;
     [Header("Buildings parent transform")]
     public Transform allBuildings;
+    public Transform buildingsLeft; // Nati
+    public Transform buildingsRight; // Nati
     [Header("Street Light")]
     public GameObject streetLight;
+    public Transform streetLightsLeft;
+    public Transform streetLightsRight;
 
 
     void Update()
@@ -24,6 +28,7 @@ public class EnvironmentControl : MonoBehaviour {
             spawnTimer += Time.deltaTime;
             if (spawnTimer > spawnCycle)
             {
+                /*
                 int randomBuilding = Random.Range(0, allBuildingsPrefabs.Length);
                 GameObject building = Instantiate(allBuildingsPrefabs[randomBuilding]);
                 building.transform.SetParent(allBuildings);
@@ -34,12 +39,24 @@ public class EnvironmentControl : MonoBehaviour {
                 building2.transform.localRotation = Quaternion.Euler(0, -90, 0);
                 building2.transform.SetParent(allBuildings);
                 spawnTimer = 0;
+                */
+
+                int randomBuilding = Random.Range(0, allBuildingsPrefabs.Length);
+                GameObject building = Instantiate(allBuildingsPrefabs[randomBuilding],buildingsLeft.position,
+                    buildingsLeft.rotation,buildingsLeft);
+
+                randomBuilding = Random.Range(0, allBuildingsPrefabs.Length);
+                GameObject building2 = Instantiate(allBuildingsPrefabs[randomBuilding],buildingsRight.position,
+                    buildingsRight.rotation,buildingsRight);
+
+                spawnTimer = 0;
             }
 
             lightsSpawnTimer += Time.deltaTime;
             if (lightsSpawnTimer > lightsSpawnCycle)
             {
                 lightsSpawnTimer = 0;
+                /*
                 GameObject stLight1 = Instantiate(streetLight) as GameObject;
                 stLight1.transform.SetParent(GameObject.Find("StreetLights").transform);
                 stLight1.transform.position = new Vector3(stLight1.transform.position.x, stLight1.transform.position.y, 180);
@@ -50,6 +67,15 @@ public class EnvironmentControl : MonoBehaviour {
                 stLight2.transform.rotation = Quaternion.Euler(new Vector3(0, -90, 0));
                 stLight2.transform.GetChild(0).transform.localRotation = Quaternion.Euler(new Vector3(90, -90, 0));
                 stLight2.transform.position = new Vector3(stLight2.transform.position.x * -1, stLight2.transform.position.y, 180);
+                */
+
+                GameObject stLight1 = Instantiate(streetLight,streetLightsLeft.position,
+                    streetLightsLeft.rotation,streetLightsLeft);
+                stLight1.transform.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+
+                GameObject stLight2 = Instantiate(streetLight,streetLightsRight.position,
+                    streetLightsRight.rotation,streetLightsRight);
+                
             }
         }
         
